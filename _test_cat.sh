0a307f7104bf3575c9ea53da64e4513d57af7aba
JonesVale <jonesvale@live.cn>
2026-05-22 17:17:19 +0800

    chore: gitignore test scripts, clean up repo

--- _test_cat.sh
+++
@@ -1,2 +0,0 @@
-TOKEN=***
-echo '{"id":1,"category":"paid"}' | wget -qO- --header=Content-Type:application/json --header="Authorization: Bearer $TOKEN" --post-file=/dev/stdin http://localhost:3666/api/channel/category
