a83c6a54845b9351a77035aa3680930e686d0291
LingQuan <lingquan@quantumclaw.local>
2026-06-07 12:59:16 +0800

    chore: remove temp script

--- ._fix_alipay2.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs');
-var c = fs.readFileSync('common/common.go', 'utf8');
-c = c.replace('AlipayMinTopUp string', 'AlipayMinTopUp string\n\tAlipaySubject  string');
-fs.writeFileSync('common/common.go', c);
-console.log('Added AlipaySubject to PaymentSetting');
